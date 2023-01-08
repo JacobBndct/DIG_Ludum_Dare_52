343a64c3d61ad3ddecd44d9bf63df2d9a1352da2
JacobBndct <66215501+JacobBndct@users.noreply.github.com>
2023-01-08 16:49:31 -0400

    Highscore system

--- Assets/Scripts/GameState/ScoreManager.cs
+++ Assets/Scripts/GameState/ScoreManager.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    public float CurrentScore { get; private set; }
+    [SerializeField] public float CurrentScore/* { get; private set; }*/;
+    [SerializeField] public float HighScore/* { get; private set; }*/;
     public bool active = true;
+
+    [SerializeField] private int numberOfIterations;
+    [SerializeField] private float UpdateSpeed;
 
     public static ScoreManager Instance { get; private set; }
     private void Awake()
@@ -24,12 +28,26 @@ public class ScoreManager : MonoBehaviour
     private void Start()
     {
         CurrentScore = 0f;
-        DontDestroyOnLoad(gameObject);
     }
 
-    //Add Score when Alien dies and/or other event happens
-    public void AddScore(float score)
+    private void Update()
     {
-        CurrentScore += score;
+        if (HighScore <= CurrentScore)
+        {
+            HighScore = CurrentScore;
+        }
+    }
+
+    public IEnumerator AddScore(float score)
+    {
+        float iterationLength = UpdateSpeed / numberOfIterations;
+        float iterationScore = (2 * score) / (numberOfIterations * (numberOfIterations + 1));
+
+        for (int i = numberOfIterations; i > 0; i--)
+        {
+            CurrentScore += i * iterationScore;
+
+            yield return new WaitForSeconds(iterationLength);
+        }
     }
 }
